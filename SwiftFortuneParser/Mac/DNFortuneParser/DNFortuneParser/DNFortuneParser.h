c50bc7686b94971b85527a3e19f73dc2fa082a9a
David Nedrow <dnedrow@me.com>
2016-12-29 21:46:48 -0500

    Basic project structure
    Set up project structure.

---
+++ SwiftFortuneParser/Mac/DNFortuneParser/DNFortuneParser/DNFortuneParser.h
@@ -0,0 +1,19 @@
+//
+//  DNFortuneParser.h
+//  DNFortuneParser
+//
+//  Created by David Nedrow on 12/29/16.
+//  Copyright © 2016 David Nedrow. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for DNFortuneParser.
+FOUNDATION_EXPORT double DNFortuneParserVersionNumber;
+
+//! Project version string for DNFortuneParser.
+FOUNDATION_EXPORT const unsigned char DNFortuneParserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DNFortuneParser/PublicHeader.h>
+
+
